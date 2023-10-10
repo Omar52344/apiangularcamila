d06610f3e013f9f360c2902a6fcbc7a101d4a679
Mariacamila05 <mendezcamila935@gmail.com>
2023-10-09 21:12:34 -0500

    nuevo controlador

---
+++ apiangularcamila/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using apiangularcamila.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiangularcamila.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoginController>/5
+        [HttpPost("Login")]
+        public bool Post([FromBody] User user)
+        {
+            var Usuario = user;
+
+            return true;
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- apiangularcamila/Program.cs
+++ apiangularcamila/Program.cs
@@ -3,11 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowMyOrigin",
+        builder => builder.AllowAnyOrigin() // Para desarrollo. En producción, reemplaza por el dominio específico de tu cliente.
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AllowMyOrigin");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
